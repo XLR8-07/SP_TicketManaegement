f73938eaaa4ef27a3a37e1c0cd4e2348a9544a14
XLR8-07 <joyashikur@gmail.com>
2020-03-27 01:59:19 +0600

    json for ticket created

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace WebApplication1
             services.AddControllers();
 
             services.AddTransient<UserRepository>();
+            services.AddTransient<TicketRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
